b32dda1dc538fda1ed3093402af789daf80c28ac
Rana Hmaidan <32235256+Rana-Hmaidan@users.noreply.github.com>
2022-08-14 19:52:45 +0300

    C# basics
    C# basics

--- Program.cs
+++ Program.cs
@@ -1,62 +1,101 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace hellohuman
+namespace Giraffe
 {
-class program
-{
-    static void Main()
+    class Program
     {
-       
-       String hello = "Hello, World";
-       Console.WriteLine(hello);
+        static void Main(String[] args)
+        {
 
-       //String hello2 = null;
-        #nullable enable
-        String? hello2 = null; //null safe 
+            
+            //Console.WriteLine("hello world");
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+
+        //variables
+            String charName="Rana";
+            int charAge;
+            charAge=25;
+            Console.WriteLine("Name: "+charName+", Age: "+charAge);
+
+            charName="Maher";
+            Console.WriteLine("Name: "+charName+", Age: "+charAge);
+
+        //data types
+            String phrase="some academy";
+            Char grade='K';
+            int age=26;
+            //float, double, decimal
+            double gpa=3.1;
+            bool isMale= true;
+            //constant
+            Console.WriteLine("k");
+            Console.WriteLine(30);
+
+        //work with strings
+            Console.WriteLine("Some\nAcademy"); //new line ("\n")
+
+            String somephrase="some academy"+"is cool";
+            Console.WriteLine(somephrase);
+
+            // String methods
+            Console.WriteLine(somephrase.ToUpper());
+            Console.WriteLine(somephrase.Contains("academy"));
+
+            //start with index 012...i
+            Console.WriteLine(somephrase[2]); //print m
+
+            //to return indexof 
+            Console.WriteLine(somephrase.IndexOf("academy"));
+
+            //return Substring(index)
+            //Substring(index, how char to grab)
+            Console.WriteLine(somephrase.Substring(8, 3));
+
+        //numbers
+            // + - * /
+            Console.WriteLine(5 + 3); //return 8
+            Console.WriteLine(5 - 3); //return 2
+            Console.WriteLine((4 + 2) * 2); //return 12
+
+            //decimal number
+            Console.WriteLine(5 / 2.0); // return 2.5
+
+            //Math 
+            Console.WriteLine(Math.Pow(3, 2)); 
+            Console.WriteLine(Math.Max(3, 2)); 
+
+    //take values from user 
+        Console.Write("Enter your name:");
+        String username = Console.ReadLine();
+        Console.WriteLine("hello " + username);
+    
+        Console.Write("Enter your age:");
+        String userage = Console.ReadLine();
+        Console.WriteLine("hello " + username + "your age is "+userage);
+
+
+    //convert string "45" to decimal (to int ) ..
+        int num = Convert.ToInt32("45");
+
+    //calculator
+        Console.Write("Enter a number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter another number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(num1 + num2);
+
+    Console.ReadLine();
+        }
     }
-}
-
-class Humanid
-{
-   public Humanid()  //constructor
-   {
-     Console.WriteLine("first , a live ");
-   }
-
-    ~Humanid()  //destructor
-    {
-     Console.WriteLine("final");
-    }
-
-    public String Name  //Auto-implemented properties 
-    {get; set;}
-
-    //methods
-    private void sayHello()
-    {
-       String hello = "Hello, World";
-       Console.WriteLine(hello);
-    }
-
-    private void fun() //LAMDDA expressions and functional programming patterns 
-    {
-        Func<int, int> doubleIt= x=> x*2;
-        doubleIt(23); // ==46
-
-    }
-
-    async void myJob()
-    {
-        var a = await SlowTask();
-    }
-
-}
-
-class SuperHuman:Humanid //inheritance 
-{
-    public override void walk(){   //polymorphism
-        Console.WriteLine("walk faster!");
-    }
-}
-
 }
