e1e1c6205c906b29bf40b18bd6ff51b189689967
Rana Hmaidan <32235256+Rana-Hmaidan@users.noreply.github.com>
2022-08-16 15:52:31 +0300

    assignment1
    assignment1

--- Program.cs
+++ Program.cs
@@ -1,21 +1,9 @@
-using System.Text.Json;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
-
 //creates an HTTP GET endpoint / which returns Hello World!
 app.MapGet("/", () => "Hello World!");
-
 //Returns a JSON with your name when you ask to GET the /whoami resource/route on it
-app.MapGet("/whoami", () => Results.Json(new webUser {
-                      Name = "Rana Hmaidan" }, options));
+app.MapGet("/whoami", () => Results.Json(new { Name = "Rana Hmaidan"}));
 
-app.Run();   //run the app
-
-class webUser
-{
-    //public fields instead of properties.
-    public string? Name { get; set; }
-}
+app.Run();  //run the app
